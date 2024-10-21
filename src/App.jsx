7eff98b0f5a6967220a79f2be35564413169345c
samito <martinsamuelpp@gmail.com>
2024-10-21 11:55:41 -0600

    2.11: La Agenda Telefónica Paso 6 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
+import axios from "axios";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/persons").then((response) => {
+      const dataPersons = response.data;
+      setPersons(dataPersons);
+      setOriginalPersons(dataPersons)
+      console.log(persons);
+    });
+  }, []);
+
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
-  const [originalPersons, setOriginalPersons] = useState([...persons]);
+  const [originalPersons, setOriginalPersons] = useState(persons);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -46,8 +52,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter searchTerm={searchTerm} setSearchTerm={setSearchTerm} originalPersons={originalPersons} setPersons={setPersons} />
-      <PersonForm addPerson={addPerson} handleNumber={handleNumber} handlePerson={handlePerson} newName={newName} newNumber={newNumber} />
+      <Filter
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        originalPersons={originalPersons}
+        setPersons={setPersons}
+      />
+      <PersonForm
+        addPerson={addPerson}
+        handleNumber={handleNumber}
+        handlePerson={handlePerson}
+        newName={newName}
+        newNumber={newNumber}
+      />
       <Persons persons={persons} />
     </div>
   );
