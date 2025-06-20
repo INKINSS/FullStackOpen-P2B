c7016f2ead598cd1b72184a1fb819743dc1dad68
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-06-20 10:21:10 -0600

    3.11 Agenda Telefónica Full Stack

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Persons from "./components/Persons";
 const App = () => {
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/persons").then((response) => {
+    axios.get("https://node-express-6vxo.onrender.com/api/persons").then((response) => {
       const dataPersons = response.data;
       setPersons(dataPersons);
       setOriginalPersons(dataPersons)
