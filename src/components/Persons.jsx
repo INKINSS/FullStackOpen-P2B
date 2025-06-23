1f3bb7fb8146fcf05633c8e20608daab53deea35
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-06-22 19:53:32 -0600

    fix: correct formatting and improve readability in Persons component

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Persons = ({ persons }) => {
   return (
     <main>
-        <h2>Numbers</h2>
+      <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.id}>{person.name}: {person.number}</p>
+        <p key={person._id}>
+          {person.name} {person.number}
+        </p>
       ))}
     </main>
-  )
-}
+  );
+};
 
-export default Persons
+export default Persons;
