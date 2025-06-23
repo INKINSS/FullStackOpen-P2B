ef9b7b77272c9d0627677578557218cea2c45e77
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-06-22 19:53:25 -0600

    refactor: update API calls to use async/await and improve code readability

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import axios from "axios";
 import Persons from "./components/Persons";
 
 const App = () => {
-
   useEffect(() => {
-    axios.get("https://node-express-6vxo.onrender.com/api/persons").then((response) => {
-      const dataPersons = response.data;
-      setPersons(dataPersons);
-      setOriginalPersons(dataPersons)
-      console.log(persons);
-    });
+    axios
+      .get("https://node-express-6vxo.onrender.com/api/persons")
+      .then((response) => {
+        const dataPersons = response.data;
+        setPersons(dataPersons);
+        setOriginalPersons(dataPersons);
+        console.log(persons);
+      });
   }, []);
 
   const [persons, setPersons] = useState([]);
@@ -21,23 +22,19 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [originalPersons, setOriginalPersons] = useState(persons);
 
-  const addPerson = (event) => {
-    event.preventDefault();
-    const personObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1,
-    };
-
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} ya existe en la agenda`);
-    } else if (persons.some((person) => person.number === newNumber)) {
-      alert(`${newNumber} ya existe en la agenda`);
-    } else {
-      setPersons(persons.concat(personObject));
-      setOriginalPersons(persons.concat(personObject));
+  const addPerson = async(e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.post("https://node-express-6vxo.onrender.com/api/persons", {
+        id: persons.length + 1,
+        name: newName,
+        number: newNumber,
+      });
+      setPersons(persons.concat(response.data));
       setNewName("");
       setNewNumber("");
+    } catch (error) {
+      console.log(error);
     }
   };
 
