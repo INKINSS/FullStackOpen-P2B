f55924f888f004630f8172a8c51f7cfd48ec2c2a
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-12 09:32:58 -0600

    feat: añade el componente login y estado de username y password

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,38 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import axios from "axios";
 import Persons from "./components/Persons";
+import { Login } from "./components/Login";
 
 const App = () => {
-  useEffect(() => {
-    axios
-      .get("https://node-express-6vxo.onrender.com/api/persons")
-      .then((response) => {
-        const dataPersons = response.data;
-        setPersons(dataPersons);
-        setOriginalPersons(dataPersons);
-        console.log(persons);
-      });
-  }, []);
 
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [error, setError] = useState(null);
-  const [newNumber, setNewNumber] = useState("");
-  const [searchTerm, setSearchTerm] = useState("");
-  const [originalPersons, setOriginalPersons] = useState(persons);
+    const [persons, setPersons] = useState([]);
+    const [newName, setNewName] = useState("");
+    const [error, setError] = useState(null);
+    const [newNumber, setNewNumber] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
+    const [originalPersons, setOriginalPersons] = useState(persons);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(() => {
+        const fetchPersons = async () => {
+          try {
+            const response = await axios.get("https://fullstackopen-back-ikel.onrender.com/api/persons");
+            setPersons(response.data);
+            setOriginalPersons(response.data);
+          } catch (err) {
+            console.error("Error en fetchPersons:", err);
+          }
+        };
+        fetchPersons();
+      }, []);
+
+      const handleLogin = (e) => {
+        e.preventDefault();
+        console.log("Login:", username, password);
+      }
+      
+
 
   const addPerson = async (e) => {
     try {
@@ -54,6 +67,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Login username={username} setUsername={setUsername} password={password} setPassword={setPassword} handleLogin={handleLogin}/>
       <Filter
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm}
